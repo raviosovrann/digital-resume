1b222d5c6a924f5f587de641d2f7dd36674c1020
raviosovrann <raviosovrann@proton.me>
2024-01-22 18:12:54 -0500

    Changed resume, added lottie json files to load them locally, made changes to UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
-import requests
+import json
 
-
+# Loading JSON file 
+def load_lottie_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+        
 def main():
     # Page configs (tab title, favicon)
     st.set_page_config(
@@ -22,19 +26,14 @@ def main():
         
     # Profile image file
     img = Image.open('assets/profile.png')
-    
-    # Load lottie files 
-    def load_lottie_animation(url):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
+
     
     # Animation data
-    animation_1 = load_lottie_animation('https://lottie.host/22002488-7f65-4420-8e8b-47115ef50dce/JdMtyZ7Jr2.json')
-    animation_2 = load_lottie_animation('https://lottie.host/728da99b-ae72-43b2-856c-292d4245c04c/uhgkxqXby6.json')
-    animation_3 = load_lottie_animation('https://lottie.host/5cd3e33b-ecf8-48b5-bac8-fbd9121d4e27/ibUGY7pWi2.json')
-    animation_4 = load_lottie_animation('https://lottie.host/73bfc708-f0fb-4514-9d8e-2f34357c9017/Hg9jvMHGXu.json')
+    animation_1 = load_lottie_json('assets/Animation - 1705722032566.json')
+    animation_2 = load_lottie_json('assets/Animation - 1705722884068.json')
+    animation_3 = load_lottie_json('assets/Animation - 1705725358517.json')
+    animation_4 = load_lottie_json('assets/Animation - 1705726809165.json')
+    
     
     # Social links dictionary     
     socials = {
@@ -49,13 +48,13 @@ def main():
     
     # Title profile image and social icons
     with st.container():
-        info_column, animation_column = st.columns((1, 1))
+        info_column, animation_column = st.columns((1, 3))
         with info_column:
             st.header("Hi, I am Anvar :wave:")
             st.write('')
             st.image(img)
             st.write(f"""
-                    <div style="display: flex; margin-bottom: 20px;">
+                    <div style="display: flex; margin-bottom: 15px;">
                         {''.join(socials_html)}
                     </div>""", 
                     unsafe_allow_html=True)
@@ -73,67 +72,74 @@ def main():
             )
             
         with animation_column:
-            st_lottie(animation_source=animation_1, speed=1, quality='high', height=500, width=600)
+            st_lottie(animation_source=animation_1, speed=1, quality='high', height=400, width=400)
     st.divider()
         
     # Experience
     with st.container():
         st.header("Experience")
-        left_col, middle_col, right_col = st.columns(3)
-        with left_col:
-            st.write('QA Analyst at DASH Financial Technologies', unsafe_allow_html=True)
-            st.text('Jersey City, NJ')
-            st.text('03/2022 – Present')
-            st.info('''
-                    -	Perform Smoke, Functional, Integration and Regression Testing in UAT and DEV environments
-                    -	Develop and execute test plans, test cases and test scripts to ensure the quality of Software and Web Platform
-                    -	Identified, documented, and reported software bugs via Jira, worked closely with Dev team to resolve them
-                    -	Automated test cases using Python reducing testing time drastically and improving overall efficiency 
-                    -	Collaborated with cross-function teams to ensure the successful deployment of new features and updates
-                    -	Participated in Agile meetings such as: sprint planning, daily stand ups and weekly reviews
-                     ''')
-        with middle_col:
-            st.write('Data Analyst at SCOUTFY', unsafe_allow_html=True)
-            st.text('São Paulo, Brazil')
-            st.text('04/2021 – 03/2022')
-            st.info('''
-                    -	Data mining through the collection of data of the world sports ecosystem
-                    
-                    -	Web Scraping with Selenium, Beautiful Soup, Scrapy to extract information from Web App UI
-                    
-                    -	Data Extraction from Web and Cloud systems (ETL)
-                    
-                    -	Data Transformation, removal of duplicates and Null values from data
-                    
-                    -  	Data Loading into CSV and JSON file formats
+        st.write('')
+        
+        st.subheader('_QA Analyst at DASH Financial Technologies | Jersey City, NJ_')
+        st.caption('_03/2022 – Present_')
+        st.info('_DASH Financial Technologies is a leading trading technology provider that offers customizable solutions to help clients grow and maximize revenue_')
+        st.text('''
+                - Led comprehensive testing (Smoke, Functional, Integration, Regression) in UAT and DEV environments to fortify software quality.
+                
+                - Developed and executed test plans, cases, and scripts, identifying, and resolving software bugs with precision.
+                
+                - Automated test cases with Python, reducing testing time by 35% and enhancing overall efficiency.
+                
+                - Collaborated cross-functionally, ensuring seamless deployment of new features and updates, streamlining development.
+                
+                - Proficient in Agile methodology, delivering exemplary client support, and conducting efficient database and server testing.
                     ''')
-        with right_col:
-            st.write('Internship at Boston Consulting Group', unsafe_allow_html=True)
-            st.text('Virtual Internship')
-            st.text('12/2019 – 03/2021')
-            st.info('''
-                    -	Fundamental understanding between the types of markets in which investments are traded.
-                    
-                    -	Differentiate different US regulatory agencies for stock market
-                    
-                    -	Understand the difference between the various types of brokerage offerings.
-                    
-                    -	Understand municipal bonds to corporate bonds as investment options
-                    
-                    -	Arbitrage in the foreign exchange market
-                    
-                    -	Understanding of the factors involved in commodities pricing.
-                    ''')
+        st.write('###')
+        
+    with st.container():
+        st.subheader('_Data Analyst at SCOUTFY | São Paulo, Brazil_')
+        st.caption('_04/2021 – 03/2022_')
+        st.info('_SCOUTFY is a company that provides innovative and efficient solutions for the sportscience industry, focusing on collecting and optimizing athlete data_')
+        st.text('''
+                - Conducted extensive data mining within the global sports ecosystem, strategically collecting pertinent data for analysis.
+                
+                - Applied advanced web scraping techniques using Selenium, Beautiful Soup, and Scrapy to extract valuable information from Web App UI.
+                
+                - Proficiently performed Data Extraction from Web and Cloud systems using ETL (Extract, Transform, Load) processes.
+                
+                - Implemented meticulous Data Transformation techniques, including the removal of duplicates and null values, to ensure data integrity and accuracy.
+                
+                - Executed seamless data loading into CSV and JSON file formats, facilitating efficient data storage and retrieval processes.
+                ''')
+        st.write('###')
+        
+    with st.container():
+        st.subheader('_Internship at Boston Consulting Group | Virtual Internship_')
+        st.caption('_12/2019 – 03/2021_')
+        st.info('_Boston Consulting Group (BCG) is a global management consulting firm that partners with leaders in business and society to tackle any financial challenges_')
+        st.text('''
+                - Demonstrated a profound comprehension of the diverse markets in which investments are traded, discerning key nuances between various market types.
+                
+                - Distinguished and comprehensively understood the regulatory frameworks set forth by different US regulatory agencies governing the stock market.
+                
+                - Exhibited an in-depth understanding of the distinct offerings provided by various brokerage entities, showcasing expertise in brokerage services.
+                
+                - Leveraged knowledge to differentiate between municipal bonds and corporate bonds, presenting insightful investment options to stakeholders.
+                
+                - Applied strategic insight to navigate arbitrage opportunities in the foreign exchange market, contributing to effective financial optimization.
+                
+                - Displayed analytical acumen in comprehending the multifaceted factors influencing commodities pricing, enhancing investment landscape.
+                ''')
     st.divider()
     
     # Tech Stack
     with st.container():
         st.header('Tech Stack')
-        stack_animation, info = st.columns((1, 1))
+        stack_animation, info = st.columns((1, 3))
         with stack_animation:
-            st_lottie(animation_source=animation_2, speed=1, quality='high', height=300, width=600)
+            st_lottie(animation_source=animation_2, speed=1, quality='high', height=400, width=300)
         with info:
-            st.write('''
+            st.text('''
                         - Python / Django / Flask / Streamlit / Flask
                         - Mac OS / Windows / Linux
                         - Jira / Confluence / Zephyr
@@ -146,20 +152,20 @@ def main():
                         - FIX Protocol
                         - Microsoft Office Suite 
                         - Postman
-                    ''', unsafe_allow_html=True)
+                    ''')
         st.divider()
     
     # Certifications
     with st.container():
         st.header('Certifications')
-        cert_animation, certs = st.columns((1, 1))
+        cert_animation, certs = st.columns((1, 3))
         with cert_animation:
-            st_lottie(animation_source=animation_3, speed=1, quality='high', height=300, width=600)
+            st_lottie(animation_source=animation_3, speed=1, quality='high', height=400, width=300)
         with certs:
             st.write('##')
             st.write('##')
             st.write('##')
-            st.write('''
+            st.text('''
                      - Google - Crash Course on Python: {url1}
                      
                      - Google - Using Python to Interact with OS: {url2}
@@ -174,20 +180,20 @@ def main():
     # Education
     with st.container():
         st.header('Education')
-        education, edu_animation = st.columns((1, 1))
+        edu_animation, education = st.columns((1, 3))
+        with edu_animation:
+            st_lottie(animation_source=animation_4, quality='high', height=400, width=300) 
         with education:
             st.write('##')
             st.write('##')
             st.write('##')
-            st.write('''
+            st.text('''
                      
-                     * CUNY Hunter College – Computer Science (2017 – 2021)
+                     - CUNY Hunter College – Computer Science (2017 – 2021)
                      
-                     * CUNY Baruch College – Data Analysis (2023 – present)
+                     - CUNY Baruch College – CyberSecurity (2023 – present)
                      
                      ''')
-        with edu_animation:
-            st_lottie(animation_source=animation_4, speed=1, quality='high', height=300, width=600) 
 
 
 if __name__ == "__main__":
